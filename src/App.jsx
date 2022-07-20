09cad924c18863460d265de60e37dc73cec4cf0f
Marc <fischerbootcamp@gmail.com>
2022-07-20 18:55:14 +0800

    Created react components and component scss

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="App">
+
+ <Topbar />
+   <div className="sections">
+    <Intro/>
+    <Portfolio/>
+    <Testimonials/>
+    <Works/>
+    <Contact/>
+    </div> 
+
+    </div>
+  );
+}
+
+export default App;
